97a200c95f46df1932ba3a8c682fb7fbf94d0056
triwa <triwa@wowway.com>
2018-05-21 14:07:32 -0500

    Just need summaries and proper artwork now

---
+++ WooleyWorld_Website/Controllers/AboutUsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WooleyWorld_Website.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
